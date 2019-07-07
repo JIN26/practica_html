8b9de8ec1174f8468e5648263e2f5127ae85144f
JIN26 <Angelmantilla26@gmail.com>
2019-07-07 17:46:56 -0500

    orden y separacion de documentos

--- public_html/js/comportamiento.js
+++ public_html/js/comportamiento.js
@@ -3,8 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-var nombre = prompt("Cual es tu nombre?");
-
-alert("Tu nombre es: nombre");
+var peso = parseFloat(prompt("¿Cual es tu peso?"));;
+//Array es una cadena de variables en este caso string 
+var planeta = new Array("Marte","Jupiter","este planeta no existente");
+var lugar=parseInt(prompt("Elige tu planeta\n\n1 es "+planeta[0]+" , 2 es "+planeta[1]));
+//esto seria un Array de float 
+var gravedad = new Array(9.8,3.7,24.8);
+var pesoFinal;
+//otra forma de hacer una estructura condicional
+switch(lugar){
+    case 1:
+        pesoFinal = parseFloat(peso * gravedad[1] / gravedad[0]);
+    break;
+    case 2:
+        pesoFinal = parseFloat(peso * gravedad[2] / gravedad[0]); 
+    break;
+    default:
+        lugar=3;
+        pesoFinal = "No calculable";
+    break;
+}
+//el (--lugar) == (lugar-1)
+var ruta = window.location;
+console.log(ruta);
+document.write("Tu peso en "+planeta[--lugar]+" es  <strong> "+ pesoFinal + " kilos</strong>"); 
 
